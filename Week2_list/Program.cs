5eca040c3ce97f79bc2150cf115424a1aa0bd371
felix diaz <felixcamposlu@gmail.com>
2025-04-08 09:23:57 -0500

    added week2_list to repository

---
+++ Week2_list/Program.cs
@@ -0,0 +1,58 @@
+﻿
+List<Libro> libros = new List<Libro>()
+{
+    new Libro
+    {
+        Nombre = "el hobbit",
+        Precio = 70,
+        Stock = 10
+    },
+
+    new Libro
+    {
+        Nombre = "el señor de los anillos la comunidad del anillo" ,
+        Precio = 120.40,
+        Stock = 10
+    },
+    new Libro
+    {
+        Nombre = "la torre oscura vol 1" ,
+        Precio = 20,
+        Stock = 5
+    },
+    new Libro
+    {
+        Nombre = "el señor de los anillos" ,
+        Precio = 120.40,
+        Stock = 10
+    },
+    new Libro
+    {
+        Nombre = "el señor de los anillos la 2 torres" ,
+        Precio = 120.40,
+        Stock = 10
+    }
+};
+
+
+var filterStock = libros.Where(libro => libro.Stock > 10).ToList();
+
+foreach (var item in filterStock)
+{
+    Console.WriteLine($"stock con mas de 10 {item}");
+}
+
+var filterPrice = libros.Where(libro => libro.Precio > 10.99).ToList();
+
+var findIndex = libros.FindIndex(libro => libro.Nombre.StartsWith("El"));
+
+
+class Libro
+{
+    public string Nombre { get; set; }
+    public double Precio { get; set; }
+    public int Stock { get; set; }
+}
+
+
+
---
+++ week1_ejecicios/Program.cs
@@ -0,0 +1,223 @@
+﻿
+/*
+Console.WriteLine("encontrar el mayor numero");
+
+Console.WriteLine("ingresa 3 numeros");
+
+Console.Write("ingresa el primer numero: ");
+
+int num1 = Convert.ToInt16(Console.ReadLine());
+Console.WriteLine();
+
+Console.Write("ingresa el segundo numero: ");
+int num2 = Convert.ToInt16(Console.ReadLine());
+
+Console.WriteLine();
+
+Console.Write("ingresa el tercer numero: ");
+int num3 = Convert.ToInt16(Console.ReadLine());
+int largestNumer ;
+
+if(num1 < num2)
+{
+    largestNumer = num2;
+
+    if(num2 < num3)
+    {
+        largestNumer = num3;
+        Console.WriteLine($" el mayor numero{largestNumer}");
+    }
+    else
+    {
+
+        Console.WriteLine($" el mayor numero{largestNumer}");
+    }
+    
+}
+else
+{
+    if(num1 > num3)
+    {
+        largestNumer = num1;
+        Console.WriteLine($" el mayor numero{largestNumer}");
+    }
+    else
+    {
+        largestNumer = num3;
+        Console.WriteLine($" el mayor numero{largestNumer}");
+    }
+}
+
+
+Console.WriteLine("calcular el area ");
+
+
+
+string inputUser = Console.ReadLine();
+
+switch(inputUser)
+{
+    case "cuadrado":
+        Console.WriteLine("ingresa los lados del cuadrado");
+
+        Console.Write("ingresa el primer lado");
+        float input = Convert.ToSingle( Console.ReadLine());
+        Console.WriteLine();
+
+        Console.Write("ingresa el segundo lado");
+        float input2 = Convert.ToSingle(Console.ReadLine());
+
+        float areaSquare = input * input2;
+
+        Console.WriteLine($"el area del cuadrado es {areaSquare}");
+
+        break;
+
+    case "circulo":
+        Console.WriteLine("ingresa el radio del circulo");
+
+        float radius = Convert.ToSingle(Console.ReadLine());
+        
+        double area = Math.PI * (radius * radius);
+
+        Console.WriteLine($"el area del circulo es {area}");
+
+        break;
+
+    case "rectangulo":
+
+        Console.WriteLine("ingresa la base y la altura del rectangulo");
+
+        Console.Write("ingresa la base del rectangulo");
+
+        float baseRectangle = Convert.ToSingle(Console.ReadLine());
+
+        Console.WriteLine("ingresa la altura del rectangulo");
+
+        float height = Convert.ToSingle(Console.ReadLine());
+
+        float areaRectagle = baseRectangle * height;
+
+        Console.WriteLine($"el area del rectangulo {areaRectagle}");
+
+
+
+        break;
+        
+}
+
+*/
+
+// imprimir cuadadro con patronde del 1 al 5
+
+int number = 890;
+
+int c = number / 100;
+int d = (number / 10) % 10;
+int u = number % 10;
+
+Console.WriteLine("centenas " + c);
+Console.WriteLine("decenas " + d);
+Console.WriteLine("unidad " + u);
+int inicio = 1;
+
+
+
+while (inicio <= 5)
+{
+
+
+    int b = 1;
+    int checkB = 1;
+    int row = 1;
+
+    int p = 1;
+
+    while (b <= 5)
+    {
+
+        while (p <= b)
+        {
+
+            if (inicio == row && b > 0)
+            {
+
+                Console.Write("* ");
+
+
+            }
+            else
+            {
+
+                row++;
+                Console.Write(b + " ");
+
+
+            }
+
+
+            p++;
+        }
+
+        b++;
+    }
+
+
+    Console.WriteLine();
+
+    inicio++;
+
+}
+
+Console.Write("ingresa un numero: ");
+
+int multiplicationTable = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i <= multiplicationTable; i++)
+{
+    int result = multiplicationTable * i;
+
+    Console.WriteLine(result);
+}
+
+Console.WriteLine();
+
+int value = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < value; i++)
+{
+    for (int j = 1; j < i; j++)
+    {
+        Console.Write(j + " ");
+
+
+    }
+
+    for (int k = 0; k < 2; k++)
+    {
+        if (i > 1) Console.Write("* ");
+
+    }
+
+    Console.WriteLine();
+}
+
+Console.WriteLine();
+
+int start = 1;
+int valuePattern = Convert.ToInt16(Console.ReadLine());
+
+while (start < valuePattern)
+{
+    Console.WriteLine();
+
+    int secondValue = 1;
+
+    while (secondValue < start)
+    {
+        Console.Write(secondValue + " ");
+        secondValue++;
+    }
+    start++;
+}
+
