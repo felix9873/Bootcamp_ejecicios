fd7ae080caf0def8393f18eda7c4d0afb900b4f8
felix diaz <felixcamposlu@gmail.com>
2025-04-08 14:19:28 -0500

    added while to add grade

--- week2_array/Program.cs
+++ week2_array/Program.cs
@@ -16,19 +16,27 @@ void AddGrades(double[] grades)
 
     for (int i = 0; i < grades.Length; i++)
     {
+        bool isValid = false;
+       
 
-        Console.Write("ingresa las notas de los estudiantes: ");
-        double inputGrades = Convert.ToDouble(Console.ReadLine());
+        while(!isValid)
+        {
+            Console.Write("ingresa las notas de los estudiantes: ");
 
-        if (inputGrades >=  0 && inputGrades <= 100)
-        {
-              grades[i] = inputGrades;
-                
+            double inputGrades = Convert.ToDouble(Console.ReadLine());
+
+            if (inputGrades >= 0 && inputGrades <= 100)
+            {
+                grades[i] = inputGrades;
+                isValid = true;
+
+            }
+            else
+            {
+                Console.WriteLine("la notas tienen que tener un valor entre 0 y 100");
+            }
         }
-        else
-        {
-            Console.WriteLine("la notas tienen que tener un valor entre 0 y 100");
-        }
+        
 
        
     }
