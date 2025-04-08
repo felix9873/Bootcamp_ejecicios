f1be8cda407bef3ffcb8cdfe83b7cfe03f7b3ad4
felix diaz <felixcamposlu@gmail.com>
2025-04-08 14:17:12 -0500

    feature add grades and display

---
+++ week2_array/Program.cs
@@ -0,0 +1,44 @@
+﻿
+
+
+
+double[] grades = new double[10];
+
+Console.WriteLine(grades[0]);
+
+AddGrades(grades);
+DisplayGrades(grades);
+
+void AddGrades(double[] grades)
+{
+    
+   
+
+    for (int i = 0; i < grades.Length; i++)
+    {
+
+        Console.Write("ingresa las notas de los estudiantes: ");
+        double inputGrades = Convert.ToDouble(Console.ReadLine());
+
+        if (inputGrades >=  0 && inputGrades <= 100)
+        {
+              grades[i] = inputGrades;
+                
+        }
+        else
+        {
+            Console.WriteLine("la notas tienen que tener un valor entre 0 y 100");
+        }
+
+       
+    }
+    Console.WriteLine();
+}
+
+void DisplayGrades(double[] grades)
+{
+    for (int i = 0; i < grades.Length; i++)
+    {
+        Console.WriteLine($"student {i + 1} grades: {grades[i]} ");
+    }
+}
