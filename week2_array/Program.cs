fc2446397edadb812d4bd41a6e3242be6b66c780
felix diaz <felixcamposlu@gmail.com>
2025-04-09 10:25:38 -0500

    added rangGrades

--- week2_array/Program.cs
+++ week2_array/Program.cs
@@ -15,7 +15,7 @@ ApprovedStudent(grades);
 
 FindMaxAndMinGrade(grades);
 
-
+RangeGrades(grades);
 
 
 
@@ -71,6 +71,7 @@ void AvegareGrades(double[] grades)
 {
     double sum = 0;
     double avegare = 0;
+
     for (int i = 0; i < grades.Length; i++)
     {
         sum += grades[i];
@@ -114,4 +115,53 @@ void FindMaxAndMinGrade(double[] grades)
     }
 
     Console.WriteLine($"la nota mas alta es: {max} y la nota minima es: {min}");
+}
+
+void RangeGrades(double[] grades)
+{
+    int[] range = new int[5];
+
+    foreach (double grade in grades)
+    {
+        if (grade < 60)
+        {
+            range[0]++;
+        }
+        else if (grade < 70)
+        {
+            range[1]++;
+        }
+        else if (grade < 80)
+        {
+            range[2]++;
+        }
+        else if (grade < 90)
+        {
+            range[3]++;
+        }
+        else
+        {
+            range[4]++;
+        }
+
+        Console.WriteLine("Distribución por rangos:");
+        Console.WriteLine($"- Reprobado (0-59): {range[0]} estudiantes");
+        Console.WriteLine($"- Suficiente (60-69): {range[1]} estudiantes");
+        Console.WriteLine($"- Bien (70-79): {range[2]} estudiantes");
+        Console.WriteLine($"- Notable (80-89): {range[3]} estudiantes");
+        Console.WriteLine($"- Excelente (90-100): {range[4]} estudiantes");
+
+        Console.WriteLine("\nGráfica de distribución:");
+        string[] etiquetas = { "Reprobado", "Suficiente", "Bien    ", "Notable ", "Excelente" };
+
+        for (int i = 0; i < range.Length; i++)
+        {
+            Console.Write($"{etiquetas[i]}: ");
+            for (int j = 0; j < range[i]; j++)
+            {
+                Console.Write("■ ");
+            }
+            Console.WriteLine($" ({range[i]})");
+        }
+    }
 }
